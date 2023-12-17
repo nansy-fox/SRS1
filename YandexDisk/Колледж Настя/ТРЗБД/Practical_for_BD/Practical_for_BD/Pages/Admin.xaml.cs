20bdce7ca31b344d350fd7e760ad21ca768f366a
Molotkova <molotkova.26@ya.ru>
2023-12-17 22:29:27 +0700

    ллл

---
+++ YandexDisk/Колледж Настя/ТРЗБД/Practical_for_BD/Practical_for_BD/Pages/Admin.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Practical_for_BD.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common.CommandTrees.ExpressionBuilder;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Practical_for_BD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Admin.xaml
+    /// </summary>
+    public partial class Admin : Page
+    {
+        public Admin()
+        {
+            InitializeComponent();
+             
+            using (var db = new Public_transportEntities1())
+            {
+                db.Genders.Load();
+                db.Routes.Load();
+                //var users = db.Users.ToList();
+                DGUsers.ItemsSource = db.Users.ToList();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Pages.Update());
+        }
+
+    }
+}
